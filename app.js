895785261a9e418314d01f9b345aa62993510c3f
vadik-rgb <trocenkovadik@gmail.com>
2020-02-14 12:00:36 +0200

    added route

--- app.js
+++ app.js
@@ -4,33 +4,11 @@ const PORT = 3001;
 
 const app = express();
 
-const database = [
-  {
-    id: 2,
-    firstName: 'Emil',
-    lastName: 'Kulmuhametov',
-  },
-  { 
-    id: 3,
-    firstName: 'Oleg',
-    lastName: 'Petrov',
-  },
-  { 
-    id: 4,
-    firstName: 'John',
-    lastName: 'Clay',
-  }
-]
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
 
-app.use('/user', (req, res) =>  {
-  const id = req.query.id;
-  const user = database.find((i) => +i.id === +id);
-  if (user) {
-    res.send(user);
-  } else {
-    res.send('Error: There is not user with such id!!!!!!');
-  }
-})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
