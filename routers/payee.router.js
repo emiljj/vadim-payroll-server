89a05a361b1944be44336fea8ba2753174635e1a
vadik-rgb <trocenkovadik@gmail.com>
2020-02-14 18:21:53 +0200

    added routing

--- routers/payee.router.js
+++ routers/payee.router.js
@@ -8,4 +8,16 @@ payeeRouter.get('/', (req, res) => {
   res.send('Hello Vasya!!!');
 });
 
+payeeRouter.post('/', (req, res) => {
+  res.send('create Vasya!!!');
+});
+
+payeeRouter.put('/', (req, res) => {
+  res.send('update Vasya!!!');
+});
+
+payeeRouter.delete('/', (req, res) => {
+  res.send('Vasya delete!!!');
+});
+
 module.exports = payeeRouter;
