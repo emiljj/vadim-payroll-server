c670798f6dc912fcc45400a31c7f6a330751c469
Emil Kulmuhametov <emilkulmuhametov@Emils-MacBook-Pro.local>
2020-02-19 17:30:34 +0200

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const payeeRouter = require('./routers/payee.router');
 const connectDB = require('./db/connection');
 const config = require('./config');
@@ -7,6 +8,7 @@ const app = express();
 
 connectDB();
 
+app.use(cors("*"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/payee', payeeRouter);
